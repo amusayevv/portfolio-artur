dc7127797506714885299283a94f33656c795d12
amusayev <alisamusa.ms@gmail.com>
2025-06-22 00:10:20 +0400

    initial commit

---
+++ src/app/_components/nav.tsx
@@ -0,0 +1,47 @@
+module.exports = {
+    theme: {
+        extend: {
+            boxShadow: {
+                "logo-inner-shadow":
+                    "1px -1px 1px 0px rgba(0, 0, 0, 0.50) inset, -1px 1px 0px 0px rgba(255, 255, 255, 0.50) inset",
+            },
+        },
+    },
+};
+
+export default function Nav() {
+    return (
+        <nav className="flex justify-between mt-4 pl-6 pr-2 bg-white/10 p-2 h-[48px] inset-shadow-2xs inset-shadow-gray-600 w-[1100px] rounded-[8px] backdrop-blur-sm fixed left-1/2 -translate-x-1/2">
+            <img src="/logo.svg" alt="" className="logo" />
+            <ul className="w-[726px] flex text-white/80 text-center">
+                <li className="w-full bg-white/10 content-center rounded-[4px]">
+                    <a href="#">Home</a>
+                </li>
+                <li className="w-full content-center">
+                    <a href="#">About</a>
+                </li>
+                <li className="w-full content-center">
+                    <a href="#">Pricing</a>
+                </li>
+                <li className="w-full content-center">
+                    <a href="#">Portfolio</a>
+                </li>
+                <li className="w-full content-center">
+                    <a href="#">Services</a>
+                </li>
+                <li className="w-full content-center">
+                    <a href="#">Reviews</a>
+                </li>
+                <li className="w-full content-center">
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+            <a
+                className="bg-white/10 text-white content-center rounded-[4px] px-6"
+                href="#"
+            >
+                Book a call
+            </a>
+        </nav>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,168 @@
-import Image from "next/image";
+const hours = String(new Date().getHours()).padStart(2, "0");
+const minutes = String(new Date().getMinutes()).padStart(2, "0");
+const currentTime: string = hours + ":" + minutes;
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+export default function Page() {
+    return (
+        <div>
+            <section
+                className="h-dvh flex w-full px-6 items-center justify-around"
+                style={{
+                    background:
+                        "radial-gradient(100% 100% at 50% 0%, #000 0%, #000 46.63%, #090828 61.06%, #4622A8 83.29%, #C5B0F8 100%)",
+                }}
+            >
+                <div className="flex flex-col items-start gap-6">
+                    <div className="flex inset-shadow-2xs inset-shadow-gray-600 rounded-full bg-white/10 text-white items-center gap-3 px-4 py-2">
+                        <div className="w-[10px] h-[10px] bg-green-400 rounded-full"></div>
+                        <p>Dubai - United Arab Emirates | {currentTime}</p>
+                    </div>
+                    <h1 className="text-white text-6xl font-semibold max-w-[1000px]">
+                        Hi, I'm Artur. UX/UI Designer Crafting Intuitive &
+                        Beautiful Experiences.
+                    </h1>
+                </div>
+                <img
+                    src="iPhone-16-Pro.png"
+                    alt="iPhone"
+                    className="w-[320px] h-auto object-contain mt-auto"
+                />
+            </section>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <section className="mt-[100px] px-6 flex flex-col gap-10">
+                <h2 className="text-[32px]">Projects</h2>
+                <div>
+                    <div className="grid grid-cols-2 gap-6">
+                        <img
+                            className="rounded-2xl"
+                            src="project1.png"
+                            alt="laptop mockup with a modern website"
+                        />
+                        <div className="flex flex-col justify-between">
+                            <div className="flex flex-col gap-4">
+                                <div className="flex gap-4">
+                                    <img src="insider.svg" alt="star logo" />
+                                    <h4 className="font-medium text-[24px]">
+                                        Insider
+                                    </h4>
+                                </div>
+                                <p className="text-white/60 leading-[150%]">
+                                    Insider is a smart dashboard that boosts
+                                    your digital experience with quick access,
+                                    skill-building tools, and an automated
+                                    Trading Bot.
+                                </p>
+                            </div>
+                            <div className="flex justify-between">
+                                <div className="flex flex-col gap-1">
+                                    <p className="text-white/60">Links</p>
+                                    <p>Notion</p>
+                                    <p>Figma</p>
+                                </div>
+                                <div className="flex flex-col gap-1">
+                                    <p className="text-white/60">Industry</p>
+                                    <p>Crypto</p>
+                                    <p>Web 3</p>
+                                </div>
+                                <div className="flex flex-col gap-1 text-right">
+                                    <p className="text-white/60">
+                                        Delieverables
+                                    </p>
+                                    <p>UX/UI design</p>
+                                    <p>Logo design</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="grid grid-cols-2 gap-6">
+                        <img
+                            className="rounded-2xl"
+                            src="project2.png"
+                            alt="laptop mockup with a modern website"
+                        />
+                        <div className="flex flex-col justify-between">
+                            <div className="flex flex-col gap-4">
+                                <div className="flex gap-4">
+                                    <img src="confero.svg" alt="star logo" />
+                                    <h4 className="font-medium text-[24px]">
+                                        Confero
+                                    </h4>
+                                </div>
+                                <p className="text-white/60 leading-[150%]">
+                                    Confero is a powerful platform for corporate
+                                    communication, offering video calls,
+                                    messaging, and collaboration tools to
+                                    enhance remote teamwork.
+                                </p>
+                            </div>
+                            <div className="flex justify-between">
+                                <div className="flex flex-col gap-1">
+                                    <p className="text-white/60">Links</p>
+                                    <a href="#">Notion</a>
+                                    <a href="#">Figma</a>
+                                </div>
+                                <div className="flex flex-col gap-1">
+                                    <p className="text-white/60">Industry</p>
+                                    <p>Corporate</p>
+                                </div>
+                                <div className="flex flex-col gap-1 text-right">
+                                    <p className="text-white/60">
+                                        Delieverables
+                                    </p>
+                                    <p>Desktop design</p>
+                                    <p>Mobile design</p>
+                                    <p>TV design</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="grid grid-cols-2 gap-6">
+                        <img
+                            className="rounded-2xl"
+                            src="project3.png"
+                            alt="laptop mockup with a modern website"
+                        />
+                        <div className="flex flex-col justify-between">
+                            <div className="flex flex-col gap-4">
+                                <div className="flex gap-4">
+                                    <img src="sbc.svg" alt="star logo" />
+                                    <h4 className="font-medium text-[24px]">
+                                        SBC admin
+                                    </h4>
+                                </div>
+                                <p className="text-white/60 leading-[150%]">
+                                    SBC Admin is a dashboard for managing
+                                    corporate communication. It provides tools
+                                    for overseeing servers, routing groups, and
+                                    logs, optimizing remote teamwork and
+                                    collaboration.
+                                </p>
+                            </div>
+                            <div className="flex justify-between">
+                                <div className="flex flex-col gap-1">
+                                    <p className="text-white/60">Links</p>
+                                    <a href="#">Notion</a>
+                                    <a href="#">Figma</a>
+                                </div>
+                                <div className="flex flex-col gap-1">
+                                    <p className="text-white/60">Industry</p>
+                                    <p>Corporate</p>
+                                </div>
+                                <div className="flex flex-col gap-1 text-right">
+                                    <p className="text-white/60">
+                                        Delieverables
+                                    </p>
+                                    <p>UX/UI design</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
